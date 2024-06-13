a9c884326bde4e869f064982ff6b8e16b8b4c291
Jaddaoui Ayoub <Jaddaoui.salim@gmail.com>
2024-06-13 10:33:32 +0200

    commit 10:33

--- src/main/java/com/example/backend/configuration/WebConfig.java
+++ src/main/java/com/example/backend/configuration/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://backend-4ke7.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(false);
